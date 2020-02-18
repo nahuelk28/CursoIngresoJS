7c7d7dc65a76090a4ca7a706e67b03248208b3d7
nahuel <julionahuelflores@gmail.com>
2020-02-18 16:14:06 -0300

    switch tps

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -3,8 +3,6 @@ function mostrar()
 {
 	//GIT funcionando
 	alert("funcionaaaaaaaa");
-	//https://forms.gle/ohEtdAwRa9pMaVc18
-
-	//https://docs.google.com/forms/d/e/1FAIpQLScYlzMSieNMk29wn9HQsUrLDiNN4pmzM_SX1IwgHnaNWf90Qg/viewform?usp=sf_link
+	
 }
 
