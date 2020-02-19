941814825041998d853b9f88093df2a6bd0a4818
nahuel <nahuel@gmail.com>
2020-02-18 22:06:39 -0300

    comiit ultimos

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,9 +4,18 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numeroIngresado;
 
+	while(repuesta=="si")
+	{
+		numeroIngresado=prompt("ingrese un numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		contador = contador +1;
+		acumulador = acumulador + numeroIngresado;
+		repuesta =prompt("ingrese 'si' o se termina");
+	}
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+   document.getElementById('suma').value=acumulador;
+   document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,25 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numeroIngresado;
+
+
 	var respuesta='si';
+	while(repuesta =="si")
+
+		numeroIngresado=pormt("ingrese un numero: ");
+		numeroIngresado=parseInt(numeroIngresado);
+		contador = contador+1;
+
+		if(numeroIngresado>0)
+		{
+			positivo=positivo+numeroIngresado;
+		}
+		if(numeroIngresado<0)
+		{
+			negativo=negativo*numeroIngresado
+		}
+
 
 
 document.getElementById('suma').value=positivo;
